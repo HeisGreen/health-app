163d841a5f816749b2793936e99189ba1507e62f
Chido <chidogreen2003@gmail.com>
2025-03-17 16:25:45 +0100

    testing 7th

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist', // Output directory for the production build
-    assetsDir: 'assets', // Directory for static assets in the build output
-    emptyOutDir: true, // Clear the output directory before building
-  },
-  server: {
-    port: 3000, // Development server port
-  },
-  publicDir: 'public', // Specify the public directory (default is 'public')
-});
+  plugins: [react(), tailwindcss(),],
+})
