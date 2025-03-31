664e7192d579fe0fb50a4ed594fd3f98a8da16d7
Chido <chidogreen2003@gmail.com>
2025-03-31 09:53:18 +0100

    Updated Sign up and login page. Both have video as background now

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -25,14 +25,14 @@ const Login = ({ setIsLoggedIn, setFirstName, setLastName }: LoginProps) => {
         password,
       });
       const token = String(response.data.responseMessage);
-      localStorage.setItem("token", token); // Store JWT in local storage
+      localStorage.setItem("token", token);
 
       const userDetailsResponse = await axiosInstance.get(
         "/user/getUsernames",
         {
           params: { email },
           headers: {
-            Authorization: `Bearer ${token}`, // Include token in the request
+            Authorization: `Bearer ${token}`,
           },
         }
       );
@@ -40,89 +40,98 @@ const Login = ({ setIsLoggedIn, setFirstName, setLastName }: LoginProps) => {
       const firstName = userDetailsResponse.data.firstName;
       const lastName = userDetailsResponse.data.lastName;
 
-      localStorage.setItem("firstName", firstName); // Store first name
-      localStorage.setItem("lastName", lastName); // Store last name
+      localStorage.setItem("firstName", firstName);
+      localStorage.setItem("lastName", lastName);
 
-      // Update state to trigger re-render
       setIsLoggedIn(true);
       setFirstName(firstName);
       setLastName(lastName);
 
-      navigate("/dashboard"); // Redirect to dashboard after login
+      navigate("/dashboard");
     } catch (err) {
       alert("Invalid email or password");
     }
   };
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <h1 className="font-bold sm:text-4xl text-2xl mt-9 items-center justify-center text-white neon-glow">
-        Log in to your account
-      </h1>
-      <motion.div
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.6, ease: "easeOut" }}
-        className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg p-6 flex flex-col justify-between items-center bg-teal-800 mt-7 rounded-lg border-2 border-white text-white shadow-lg shadow-lime-800/60"
+    <div className="relative w-full h-screen flex items-center justify-center">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
       >
-        <motion.form
-          initial={{ opacity: 0, y: -30 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.3, duration: 0.5 }}
-          className="w-full space-y-4"
-        >
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="inputClass"
-            required
-          />
-          <input
-            type="password"
-            value={password}
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            className="inputClass"
-            required
-          />
-        </motion.form>
-        <motion.button
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          onClick={handleLogin}
-          className="w-[200px] mt-4 py-2 bg-white text-teal-800 font-bold rounded hover:bg-orange-300 hover:text-white transition duration-300"
-        >
-          Login
-        </motion.button>
+        <source src="fitvid.mp4" type="video/mp4" />
+      </video>
+      <div className=" relative flex flex-col justify-center items-center min-h-screen">
+        <h1 className="font-bold sm:text-4xl text-2xl mt-9 items-center justify-center text-white neon-glow">
+          Log in to your account
+        </h1>
         <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.5, duration: 0.5 }}
-          className="flex justify-between w-full mt-4"
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.6, ease: "easeOut" }}
+          className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg p-6 flex flex-col justify-between items-center bg-teal-800 mt-7 rounded-lg border-2 border-white text-white shadow-lg shadow-lime-800/60"
         >
-          <div className="flex items-center">
-            <input type="checkbox" id="terms" className="mr-2" />
-            <label htmlFor="terms" className="text-sm">
-              Remember me
-            </label>
-          </div>
-          <p className="text-sm text-blue-300 hover:text-orange-300 cursor-pointer">
-            <Link to="/forgot-password">Forgot password?</Link>
-          </p>
+          <motion.form
+            initial={{ opacity: 0, y: -30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+            className="w-full space-y-4"
+          >
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="inputClass"
+              required
+            />
+            <input
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="inputClass"
+              required
+            />
+          </motion.form>
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            onClick={handleLogin}
+            className="w-[200px] mt-4 py-2 bg-white text-teal-800 font-bold rounded hover:bg-orange-300 hover:text-white transition duration-300"
+          >
+            Login
+          </motion.button>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+            className="flex justify-between w-full mt-4"
+          >
+            <div className="flex items-center">
+              <input type="checkbox" id="terms" className="mr-2" />
+              <label htmlFor="terms" className="text-sm">
+                Remember me
+              </label>
+            </div>
+            <p className="text-sm text-blue-300 hover:text-orange-300 cursor-pointer">
+              <Link to="/forgot-password">Forgot password?</Link>
+            </p>
+          </motion.div>
+          <motion.h3
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.6, duration: 0.5 }}
+            className="w-full mt-6 py-4 border-t border-orange-400 text-sm text-center font-bold"
+          >
+            Don&apos;t have an account?
+            <Link to="/" className="text-orange-300 ml-1 underline">
+              Sign Up
+            </Link>
+          </motion.h3>
         </motion.div>
-        <motion.h3
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.6, duration: 0.5 }}
-          className="w-full mt-6 py-4 border-t border-orange-400 text-sm text-center font-bold"
-        >
-          Don&apos;t have an account?
-          <Link to="/" className="text-orange-300 ml-1 underline">
-            Sign Up
-          </Link>
-        </motion.h3>
-      </motion.div>
+      </div>
     </div>
   );
 };
--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -31,188 +31,142 @@ const Signup = ({ setIsLoggedIn, setFirstName, setLastName }: SignupProps) => {
   };
 
   const navigate = useNavigate();
-
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    setIsLoading(true); // Start loading
+    setIsLoading(true);
     try {
-      const response = await registerUser(formData); // Call the API service
-      const token = String(response.responseCode);
-      localStorage.setItem("token", token); // Store JWT in local storage
+      const response = await registerUser(formData);
+      localStorage.setItem("token", String(response.responseCode));
       localStorage.setItem("firstName", formData.firstName);
       localStorage.setItem("lastName", formData.lastName);
-      console.log("Registration successful:", response);
       alert("Registration successful!");
-
-      // Update state to trigger re-render
       setIsLoggedIn(true);
       setFirstName(formData.firstName);
       setLastName(formData.lastName);
-
       navigate("/dashboard");
     } catch (error) {
       console.error("Registration failed:", error);
       alert("Registration failed. Please try again.");
     } finally {
-      setIsLoading(false); // Stop loading
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="flex space-x-10">
-      <div className="ml-14">
-        <p className="font-bold text-4xl mt-9 items-center justify-center text-white neon-glow">
-          Create an Account with us
-        </p>
-        <div className="w-[300px] h-[450px] sm:w-[450px] sm:h-[750px] flex flex-col justify-between items-center bg-teal-800 mt-7 rounded-lg border-2 border-white text-white shadow-lg shadow-lime-800/6-">
-          <form
-            id="signupForm"
-            className="h-52 w-full grid grid-cols-2 sm:flex sm:flex-col sm:justify-between sm:items-center mt-2 p-4 sm:space-y-12"
-            onSubmit={handleSubmit}
+    <div className="relative w-full h-screen flex items-center justify-center">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src="/fitvid.mp4"
+        autoPlay
+        loop
+        muted
+      />
+      <div className="relative bg-teal-900 bg-opacity-90 p-6 rounded-lg shadow-lg w-full max-w-lg">
+        <h2 className="text-xl sm:text-3xl font-bold text-white text-center mb-4 neon-glow">
+          Create an Account with Us
+        </h2>
+        <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-4">
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            className="inputClass"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            className="inputClass"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="inputClass"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            className="inputClass"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          <select
+            name="gender"
+            className="inputClass"
+            value={formData.gender}
+            onChange={handleChange}
+            required
           >
-            <input
-              type="text"
-              name="firstName"
-              placeholder="First Name"
-              className="inputClass"
-              value={formData.firstName}
-              onChange={handleChange}
-              required
-            />
-
-            {/* Last Name */}
-            <input
-              type="text"
-              name="lastName"
-              placeholder="Last Name"
-              className="inputClass"
-              value={formData.lastName}
-              onChange={handleChange}
-              required
-            />
-
-            {/* Email */}
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              className="inputClass"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-
-            {/* Password */}
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              className="inputClass"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-
-            {/* Gender */}
-            <select
-              name="gender"
-              className="inputClass"
-              value={formData.gender}
-              onChange={handleChange}
-              required
-            >
-              <option value="">Gender</option>
-              <option value="Female">Female</option>
-              <option value="Male">Male</option>
-            </select>
-
-            {/* Department */}
-            <select
-              name="department"
-              className="inputClass"
-              value={formData.department}
-              onChange={handleChange}
-              required
-            >
-              <option value="">Department</option>
-              <option value="HR">HR</option>
-              <option value="Backend Engineer">Backend Engineer</option>
-              <option value="Frontend Engineer">Frontend Engineer</option>
-              <option value="UI/UX Designer">UI/UX Designer</option>
-            </select>
-
-            {/* Address */}
-            <input
-              type="text"
-              name="address"
-              placeholder="Address"
-              className="inputClass"
-              value={formData.address}
-              onChange={handleChange}
-              required
-            />
-
-            {/* Phone Number */}
-            <input
-              type="text"
-              name="phoneNumber"
-              placeholder="Phone Number"
-              className="inputClass"
-              value={formData.phoneNumber}
-              onChange={handleChange}
-              required
-            />
-          </form>
+            <option value="">Gender</option>
+            <option value="Female">Female</option>
+            <option value="Male">Male</option>
+          </select>
+          <select
+            name="department"
+            className="inputClass"
+            value={formData.department}
+            onChange={handleChange}
+            required
+          >
+            <option value="">Department</option>
+            <option value="HR">HR</option>
+            <option value="Backend Engineer">Backend Engineer</option>
+            <option value="Frontend Engineer">Frontend Engineer</option>
+            <option value="UI/UX Designer">UI/UX Designer</option>
+          </select>
+          <input
+            type="text"
+            name="address"
+            placeholder="Address"
+            className="inputClass"
+            value={formData.address}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="phoneNumber"
+            placeholder="Phone Number"
+            className="inputClass"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+            required
+          />
           <button
-            form="signupForm"
-            className={`h-[38px] w-44 mt-85 bg-white text-teal-800 font-bold rounded hover:bg-orange-300 hover:text-white cursor-pointer ${
+            type="submit"
+            className={`w-full py-2 mt-4 bg-white text-teal-800 font-bold rounded hover:bg-orange-300 hover:text-white ${
               isLoading ? "opacity-50 cursor-not-allowed" : ""
             }`}
-            type="submit"
           >
             {isLoading ? "Signing Up..." : "Sign Up"}
           </button>
-          <p className="p-4 text-center text-sm">
-            Thank you for registering with us
-          </p>
-          <h3 className="w-full py-5 border-t-orange-400 border-t-[0.4px] text-sm text-center font-bold">
-            Already have an account?{" "}
-            <Link to="/login" className="text-orange-300 underline">
-              Login
-            </Link>
-          </h3>
-        </div>
-      </div>
-      <div className=" relative mt-20 h-full w-full mr-8  space-y-5 ">
-        <video
-          className="h-[770px] object-cover rounded-2xl border-4 border-teal-800"
-          src="/fitvid.mp4"
-          autoPlay
-          loop
-          muted
-        />
-
-        <div className=" absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[10px] w-[900px] mb-20 text-white">
-          <h3 className="font-bold text-5xl italic text-orange-400">
-            Take Charge of Your Health & Wellness!
-          </h3>
-          <p>
-            Your health journey starts here! Our platform helps you track
-            essential health metrics like BMI (Body Mass Index), BMR (Basal
-            Metabolic Rate), and EER (Estimated Energy Requirement) to give you
-            a deeper understanding of your body’s needs. Whether your goal is to
-            lose weight, gain muscle, or maintain a balanced lifestyle, we
-            provide the tools you need to stay on track. Sign up today to access
-            personalized insights, meal planning tips, and progress tracking
-            features—all designed to help you reach your health goals
-            efficiently. Make informed decisions about your fitness and
-            nutrition and take the first step towards a healthier, happier you!
-          </p>
-        </div>
+        </form>
+        <p className="text-center text-sm text-white mt-3">
+          Thank you for registering with us
+        </p>
+        <p className="text-center text-white mt-3">
+          Already have an account?{" "}
+          <Link to="/login" className="text-orange-300 underline">
+            Login
+          </Link>
+        </p>
       </div>
     </div>
   );
 };
+
 export default Signup;
